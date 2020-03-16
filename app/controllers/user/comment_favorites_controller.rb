91a32587e5eaff28395e3315632cbd556b5a878b
寺田　圭吾 <terada_keigo@teradakeigonoMacBook-Pro.local>
2020-03-16 21:41:32 +0900

    Add #5,#10

--- app/controllers/user/comment_favorites_controller.rb
+++ app/controllers/user/comment_favorites_controller.rb
@@ -2,17 +2,17 @@ class User::CommentFavoritesController < ApplicationController
 
 	def create
 		post = Post.find(params[:post_id])
-		comment = Comment.find(params[:comment_id])
-		comment_favorite = current_user.comment_favorites.new(user_id: current_user.id,post_id: post.id, comment_id: comment.id)
+		@comment = Comment.find(params[:comment_id])
+		comment_favorite = current_user.comment_favorites.new(user_id: current_user.id,post_id: post.id, comment_id: @comment.id)
 		comment_favorite.save
-		redirect_to request.referer
+		# redirect_to request.referer
 	end
 
 	def destroy
 		post = Post.find(params[:post_id])
-		comment = Comment.find(params[:comment_id])
-		comment_favorite = current_user.comment_favorites.find_by(user_id: current_user.id,post_id: post.id,comment_id: comment.id)
+		@comment = Comment.find(params[:comment_id])
+		comment_favorite = current_user.comment_favorites.find_by(user_id: current_user.id,post_id: post.id,comment_id: @comment.id)
 		comment_favorite.destroy
-		redirect_to request.referer
+		# redirect_to request.referer
 	end
 end
